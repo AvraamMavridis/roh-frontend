24f59970c09c9f58d13c53feb099af59e1bd92b3
Avraam Mavridis <avraammauridis@gmail.com>
2015-10-10 20:03:42 +0200

    Toggle comments button

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,20 +1,29 @@
 'use strict';
 
 let React = require('react');
+let $ = require('jQuery');
 
 require('./comments.scss');
 
 let Comments = React.createClass({
 
+  expandComments: function(id){
+    $(`#${id}`).slideToggle( "slow" );
+  },
+
   render: function() {
+      var id = ~~(Math.random() * 10000);
       return (
-          <u className="col-xs-12 comments-container">
-           <li> Something </li>
-           <li> Something </li>
-           <li> Something </li>
-           <li> Something </li>
-           <li> Something </li>
-          </u>
+          <div className="col-xs-12">
+            <div onClick={this.expandComments.bind(this, id)} className="clickable pull-right"><u><strong>Σχόλια</strong></u></div>
+            <u id={id} className="col-xs-12 comments-container">
+             <li> Something </li>
+             <li> Something </li>
+             <li> Something </li>
+             <li> Something </li>
+             <li> Something </li>
+            </u>
+        </div>
       );
   }
 });
